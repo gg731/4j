5df25601d5dd19fef1c76cf7ceae77f1fbe2bd63
137799 <g1377@protonmail.com>
2020-06-29 10:56:30 +0300

    Task 1.1

---
+++ src/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        first.start();
+        second.start();
+
+        while (true) {
+            if (first.getState() == Thread.State.TERMINATED &&
+                    second.getState() == Thread.State.TERMINATED) {
+                break;
+            }
+        }
+        System.out.println("Работа завершена");
+
+
+    }
+}
