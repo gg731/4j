5d07fe36b9c2b967f4beb1344d5095440a9170bd
137799 <g1377@protonmail.com>
2020-06-29 12:08:14 +0300

    Task 3. Прерывание нити

---
+++ src/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        try {
+            char[] s = {'\\', '|', '/'};
+            int c = 0;
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.print("\rLoading: " + s[c]);
+                c++;
+                if (c > 2) c = 0;
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.err.print("\rCurrent thread is interrupted.");
+        }
+    }
+}
