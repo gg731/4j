8410d417ca86a41724e8ad7f9dd42ed674c709c9
137799 <g1377@protonmail.com>
2020-06-29 19:54:23 +0300

    3. Immutable объекты

---
+++ src/ru/job4j/concurrent/Node.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public final class Node<T> {
+    private Node next;
+    private T value;
+
+
+    public Node(Node next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+
+}
