6d9fd36a402a342f13c922596f4fd7e89f2cfccd
137799 <g1377@protonmail.com>
2020-06-29 18:45:42 +0300

    2. Модель памяти Java

--- src/ru/job4j/concurrent/DCLSingleton.java
+++ src/ru/job4j/concurrent/DCLSingleton.java
@@ -5,7 +5,11 @@ public final class DCLSingleton {
 
     public static DCLSingleton instOf() {
         if (inst == null) {
-            inst = new DCLSingleton();
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
         }
         return inst;
     }
