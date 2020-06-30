8b17b06bae7a09ed07969d7d7f23d6052527af78
137799 <g1377@protonmail.com>
2020-06-30 12:08:15 +0300

    4. Скачивание файла с ограничением. [#315488]

--- src/main/java/ru/job4j/concurrent/FileDownload.java
+++ src/main/java/ru/job4j/concurrent/FileDownload.java
@@ -1,20 +1,21 @@
 package ru.job4j.concurrent;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
 public class FileDownload {
-    public static void main(String[] args) throws Exception {
-        String file = args[0];
-        int speedS = Integer.parseInt(args[1]);
+    private File file;
 
-        try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("new_4j.zip")) {
-            byte[] dataBuffer = new byte[speedS];
+    public FileDownload(String url, int speed, String fileName) {
+
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            byte[] dataBuffer = new byte[speed];
             int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, speedS)) != -1) {
+            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
                 long t = System.currentTimeMillis();
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
                 long s = System.currentTimeMillis() - t;
@@ -22,8 +23,28 @@ public class FileDownload {
                     Thread.sleep(1000 - s);
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+
+        this.file = new File(fileName);
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String url = args[0];
+        int speed = Integer.valueOf(args[1]);
+        String fileName = "new_file.xml";
+
+//        Test
+//        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+//        int speed = 200;
+
+        FileDownload fd = new FileDownload(url, speed, fileName);
+        fd.getFile();
+
     }
 }
