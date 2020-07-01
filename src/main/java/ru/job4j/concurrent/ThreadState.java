2f7086c2bbf75e114733e9356d3eadd3440d3d2d
137799 <g1377@protonmail.com>
2020-07-01 09:37:04 +0300

    ThreadState

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,7 +1,7 @@
 package ru.job4j.concurrent;
 
 public class ThreadState {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread first = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
@@ -12,13 +12,9 @@ public class ThreadState {
 
         first.start();
         second.start();
-
-        while (first.getState() == Thread.State.TERMINATED
-                || second.getState() == Thread.State.TERMINATED) {
-            continue;
-        }
+        first.join();
+        second.join();
 
         System.out.println("Работа завершена");
-
     }
 }
