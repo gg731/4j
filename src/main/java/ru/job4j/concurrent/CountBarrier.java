dbcaceffcacc761b0f10947bde1931001fa27605
137799 <g1377@protonmail.com>
2020-07-02 15:15:54 +0300

    0. Управление нитью через wait. [#315486]

---
+++ src/main/java/ru/job4j/concurrent/CountBarrier.java
@@ -0,0 +1,34 @@
+package ru.job4j.concurrent;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class CountBarrier {
+    @GuardedBy("this")
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            this.count++;
+        }
+    }
+
+    public void await() throws InterruptedException {
+        synchronized (monitor) {
+            if (count != total) {
+                wait();
+            } else {
+                notifyAll();
+            }
+        }
+    }
+}
