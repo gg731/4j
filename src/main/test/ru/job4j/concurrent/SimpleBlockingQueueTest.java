0e597698b6950f97c717fa37f469cd89f5bd52aa
137799 <g1377@protonmail.com>
2020-07-08 15:27:19 +0300

    SimpleBlockingQueueTest with CountDownLatch

--- src/main/test/ru/job4j/concurrent/SimpleBlockingQueueTest.java
+++ src/main/test/ru/job4j/concurrent/SimpleBlockingQueueTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,20 +18,20 @@ class SimpleBlockingQueueTest {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue();
         CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList();
         final ArrayList result = new ArrayList(Arrays.asList(0, 1, 2, 3, 4));
+        final CountDownLatch cdl = new CountDownLatch(2);
 
         Thread threadP = new Thread(
                 () -> {
                     IntStream.range(0, 5)
                             .forEach(queue::offer);
+                    cdl.countDown();
                 }
         );
-
         threadP.start();
 
         Thread threadC = new Thread(
                 () -> {
-                    while (!queue.isEmpty()
-                            || !Thread.currentThread().isInterrupted()) {
+                    while (cdl.getCount() == 2 || !queue.isEmpty()) {
                         try {
                             buffer.add(queue.poll());
                         } catch (InterruptedException e) {
@@ -38,13 +39,12 @@ class SimpleBlockingQueueTest {
                             Thread.currentThread().interrupt();
                         }
                     }
+                    cdl.countDown();
                 }
         );
 
         threadC.start();
-        threadP.join();
-        threadC.interrupt();
-        threadC.join();
+        cdl.await();
 
         assertEquals(buffer, result);
     }
