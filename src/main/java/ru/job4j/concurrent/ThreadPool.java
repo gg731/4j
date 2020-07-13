5b975c9234a7058d8f9663ed0c593160524a4345
137799 <g1377@protonmail.com>
2020-07-13 15:32:22 +0300

    1. Реализовать ThreadPool [#315481]

--- src/main/java/ru/job4j/concurrent/ThreadPool.java
+++ src/main/java/ru/job4j/concurrent/ThreadPool.java
@@ -2,25 +2,51 @@ package ru.job4j.concurrent;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ThreadPool {
     private final int size = Runtime.getRuntime().availableProcessors();
-    private final List<Thread> threads = new LinkedList<>();
+    private final List<Pool> threads = new LinkedList<>();
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
+    private boolean isStopped = false;
 
     public ThreadPool() throws InterruptedException {
-        for (int i = 0; i < size; i++) {
-            threads.add(new Thread(tasks.poll()));
-        }
+        IntStream.range(0, size).forEach(i -> threads.add(new Pool(tasks)));
 
         threads.stream().forEach(Thread::start);
     }
 
-    public synchronized void work(Runnable job) {
-        tasks.offer(job);
+    public void work(Runnable job) {
+        if (!isStopped) {
+            tasks.offer(job);
+        }
     }
 
-    public synchronized void shutdown() {
-        threads.stream().forEach(Thread::interrupt);
+    public void shutdown() {
+        isStopped = true;
+        threads.stream().forEach(t -> t.doStop());
     }
+}
+
+class Pool extends Thread {
+    private boolean isStopped = false;
+    private SimpleBlockingQueue queue;
+
+    public Pool(SimpleBlockingQueue queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        while (!isStopped) {
+            Runnable runnable = (Runnable) queue.poll();
+            runnable.run();
+        }
+    }
+
+    public void doStop() {
+        this.isStopped = true;
+        this.interrupt();
+    }
+
 }
